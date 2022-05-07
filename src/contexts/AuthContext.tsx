98bbad1038eedf8858dc7c9738033142d2de385a
Wallisson Martins <wallissonmartins37@gmail.com>
2022-05-07 14:03:28 -0300

    atualizando métodos firebase

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -62,7 +62,7 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
       })
     }
   }
-
+  
   return (
     <AuthContext.Provider value={{ user, signInWithGoogle }}>
       {props.children}
